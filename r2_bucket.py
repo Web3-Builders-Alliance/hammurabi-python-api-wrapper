3aa0d5795abc1194653f323e344bf764f1aa2ebb
jhuhnke <jessica@flipsidecrypto.com>
2023-12-31 09:56:28 -0600

    Access correct bucket for API key

--- r2_bucket.py
+++ r2_bucket.py
@@ -1,6 +1,6 @@
 import boto3
 import json
-from config import ACCESS_KEY, SECRET_KEY, BUCKET_NAME, R2_ENDPOINT_URL
+from config import ACCESS_KEY, SECRET_KEY, API_BUCKET_NAME, R2_ENDPOINT_URL
 from botocore.client import Config
 
 # Initialize R2 Client
@@ -14,12 +14,12 @@ r2_client = boto3.client('s3',
 def save_api_key_data(api_key, data):
     json_data = json.dumps(data)
     file_name = f"api_keys/{api_key}.json"
-    r2_client.put_object(Bucket=BUCKET_NAME, Key=file_name, Body=json_data)
+    r2_client.put_object(Bucket=API_BUCKET_NAME, Key=file_name, Body=json_data)
 
 def get_api_key_data(api_key):
     file_name = f"api_keys/{api_key}.json"
     try:
-        response = r2_client.get_object(Bucket=BUCKET_NAME, Key=file_name)
+        response = r2_client.get_object(Bucket=API_BUCKET_NAME, Key=file_name)
         json_data = response['Body'].read().decode('utf-8')
         return json.loads(json_data)
     except r2_client.exceptions.NoSuchKey:
